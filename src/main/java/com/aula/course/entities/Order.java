10b95750dde3ae9e98f3aac760c1c61903f4480e
Rafael Imamura Bortoni <rafaelimamura804@gmail.com>
2023-06-01 14:44:59 -0300

    Subtotal & Total methods

--- src/main/java/com/aula/course/entities/Order.java
+++ src/main/java/com/aula/course/entities/Order.java
@@ -101,6 +101,13 @@ public class Order implements Serializable {
 		return items;
 	}
 	
+	public Double getTotal() {
+		double soma = 0;
+		for(OrderItem x : items) {
+			soma = soma + x.getSubTotal();
+		}
+		return soma;
+	}
 	
 	@Override
 	public int hashCode() {
