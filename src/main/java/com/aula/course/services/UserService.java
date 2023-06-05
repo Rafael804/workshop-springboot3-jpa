5b754e42c23ad7ad5d86d8efcff0c316aa6ac461
Rafael Imamura Bortoni <rafaelimamura804@gmail.com>
2023-06-05 13:05:19 -0300

    Exception handling - delete

--- src/main/java/com/aula/course/services/UserService.java
+++ src/main/java/com/aula/course/services/UserService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.aula.course.entities.User;
@@ -31,7 +33,13 @@ public class UserService {
 	}
 	
 	public void delete (Long id) {
-		repository.deleteById(id);
+		try {
+		   repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);	
+		}catch (DataIntegrityViolationException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public User update(Long id, User obj) {
